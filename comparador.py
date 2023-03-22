40479008e25856c2ec1206ec65e8a8e4a83b8c95
OttoHapuc <dossantoshapuc@gmail.com>
2023-03-22 11:35:58 -0300

    feat: select vvalues of arquive1 for analict

--- comparador.py
+++ comparador.py
@@ -1,5 +1,13 @@
-import pandas as pd
-import datetime
+from openpyxl import workbook, load_workbook;
 
-df = pd.read_excel('indicadorProd.xlsx', sheet_name='Horimetro')
-print(df)
+mocca = load_workbook('indicadorProd.xlsx');
+
+aba_ativa = mocca['Horimetro'];
+
+for cell in aba_ativa['C']:
+    if (cell.value != None):
+        if(cell.value != "Data"):
+            if(str(cell.value).replace(' 00:00:00', "") == '2023-03-07'):
+                m1_H_inicial = aba_ativa[f'E{cell.row}'].value
+                m1_H_final = aba_ativa[f'F{cell.row}'].value
+                print(('{:.1f}'.format(m1_H_final-m1_H_inicial)))
